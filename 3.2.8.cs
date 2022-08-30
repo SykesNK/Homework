59a1e7d61a7cd5a6656ace8b00a92b79ae634129
SykesNK <ramkulov@inbox.ru>
2022-08-30 18:19:49 +1000

    Изменил знак @ на #

---
+++ 3.2.8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		const string MyName = "Jane";
+
+		Console.WriteLine(MyName);
+
+		Console.WriteLine("\t Привет Мир");
+		Console.WriteLine("\t Мне 27 лет");
+		Console.WriteLine("\t My name is \n {0}", MyName);
+		Console.WriteLine("\u0023");
+		Console.ReadKey();
+
+	}
+}
