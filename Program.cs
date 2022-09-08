d0ea25a801957b69ce5ad2faf2aceb3180c0722b
SykesNK <61617584+SykesNK@users.noreply.github.com>
2022-09-09 01:37:23 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        byte age = (byte)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.Write("Enter your birthdate: ");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Your birthdate is {0}", birthdate);
+        Console.ReadKey();
+    }
+}
